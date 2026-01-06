7a5b1aed07951601e2542913748681b3e105891f
maik001 <maikemir2003@gmail.com>
2026-01-06 00:09:30 -0300

    installing and configuring tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
