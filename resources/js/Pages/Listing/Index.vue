4d467cd47c9dad223dfd848973b25038af782068
maik001 <maikemir2003@gmail.com>
2026-01-07 00:20:42 -0300

    responsive grid boxes layout

--- resources/js/Pages/Listing/Index.vue
+++ resources/js/Pages/Listing/Index.vue
@@ -1,21 +1,24 @@
 <template>
-	<div v-for="listing in listings" :key="listing.id">
-		<div>
-			<Link :href="route('listing.show', [listing.id])">
-				<ListingComponent :listing="listing"></ListingComponent>
-			</Link>
-		</div>
-		<div>
-			<Link :href="route('listing.edit', [listing.id])">Edit</Link>
-		</div>
-		<div>
-			<Link :href="route('listing.destroy', [listing.id])" method="delete">Delete</Link>
-		</div>
+	<div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+		<Box v-for="listing in listings" :key="listing.id">
+			<div>
+				<Link :href="route('listing.show', [listing.id])">
+					<ListingComponent :listing="listing"></ListingComponent>
+				</Link>
+			</div>
+			<div>
+				<Link :href="route('listing.edit', [listing.id])">Edit</Link>
+			</div>
+			<div>
+				<Link :href="route('listing.destroy', [listing.id])" method="delete">Delete</Link>
+			</div>
+		</Box>
 	</div>
 </template>
 
 <script setup>
 import ListingComponent from '@/Components/ListingComponent.vue';
+import Box from '@/Components/UI/Box.vue';
 import { Link } from '@inertiajs/vue3';
 defineProps({
 	listings: Array,
