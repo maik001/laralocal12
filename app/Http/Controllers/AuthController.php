a9fc38444c5f66cf1df6528b186293fe11f45f15
maik001 <maikemir2003@gmail.com>
2026-01-24 17:28:47 -0300

    authcontroller and routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    // display signin form
+    public function create() {}
+
+    // handle signin form submission
+    public function store(Request $request) {}
+
+    // handle signout
+    public function destroy() {}
+}
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 
 Route::resource('listing', ListingController::class);
+
+Route::get('login', [App\Http\Controllers\AuthController::class, 'create'])->name('login');
+Route::post('login', [App\Http\Controllers\AuthController::class, 'store'])->name('login.store');
+Route::delete('logout', [App\Http\Controllers\AuthController::class, 'destroy'])->name('logout');
